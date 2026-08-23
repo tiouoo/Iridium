01398bae9105c2a5db7840c3cc5c4379f01b3683
Ktn429 <3424968114@qq.com>
2026-08-24 04:16:51 +0800

    chore: 删除无用代码

--- src/Iridium.Sample/Program.cs
+++ src/Iridium.Sample/Program.cs
@@ -1,12 +1,4 @@
 ﻿using Iridium;
 
 IridiumConfig.Configure(new IridiumContext());
-Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-while (true)
-{
-    await Iridium.Sample.MinecraftLauncher.RunAsync();
-    Console.WriteLine();
-    Console.WriteLine("Press any key to exit...");
-    Console.ReadKey();
-}
